8b4785e293ab3d2c6e693b0cbdc5d77f6025d4e4
Timmy <mr.oshnokov@gmail.com>
2021-04-16 21:04:24 +0300

    check

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ import routes from "./routers.js";
 import cors from "cors";
 
 const PORT = process.env.PORT;
-const CORS_PORT = process.env.CORS_PORT;
-const DB_URL = process.env.DB_URL;
+// const CORS_PORT = process.env.CORS_PORT;
+// const DB_URL = process.env.DB_URL;
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(routes);
 
 async function start() {
     try {
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        });
-        app.listen(CORS_PORT, function () {
-            console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
-        });
-
+        // await mongoose.connect(DB_URL, {
+        //     useNewUrlParser: true,
+        //     useFindAndModify: false,
+        //     useUnifiedTopology: true
+        // });
+        // app.listen(CORS_PORT, function () {
+        //     console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
+        // });
+        //
         app.listen(PORT, () => {
             console.log(`Launched server on port ${PORT}`);
         });
