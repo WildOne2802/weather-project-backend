235fa969396a976c56b8039eb22411065b754764
Timmy <mr.oshnokov@gmail.com>
2021-04-20 18:33:44 +0300

    hello

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import routes from "./routers.js";
 import cors from "cors";
 
 const PORT = process.env.PORT;
-// const CORS_PORT = process.env.CORS_PORT;
+const CORS_PORT = process.env.CORS_PORT;
 // const DB_URL = process.env.DB_URL;
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(routes);
 
 async function start() {
@@ -18,11 +18,11 @@ async function start() {
         //     useNewUrlParser: true,
         //     useFindAndModify: false,
         //     useUnifiedTopology: true
-        // });
-        // app.listen(CORS_PORT, function () {
-        //     console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
-        // });
-        //
+        //});
+        app.listen(CORS_PORT, function () {
+            console.log(`CORS-enabled web server listening on port ${CORS_PORT}`);
+        });
+
         app.listen(PORT, () => {
             console.log(`Launched server on port ${PORT}`);
         });
